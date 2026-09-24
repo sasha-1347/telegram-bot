4a098c3d355d3650e91deec71f100f99c1e2c8a2
sasha-1347 <sprok805@gmail.com>
2026-09-24 18:53:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = ""
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [InlineKeyboardButton(
+            "💳 Карту можна зробити тут",
+            url="https://www.privat24.ua/invite/kcxlx"
+        )]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(
+        "👋 Вітаю!\n\n"
+        "💳 Карту можна зробити тут:",
+        reply_markup=reply_markup
+    )
+
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Бот запущений...")
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
